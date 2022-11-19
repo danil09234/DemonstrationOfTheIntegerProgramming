67ac64169f711600a4ec6f4349dfff3ad3286aa0
danil09234 <dailzag05@gmail.com>
2022-11-19 03:28:37 +0100

    Code refactoring & optimization

--- main.py
+++ main.py
@@ -18,332 +18,82 @@ class Solution(NamedTuple):
     result: float | int
 
 
-def get_all_solutions_for_b(equation: Callable[[float | int, float | int], float | int],
-                            limitation: LimitationsFor2Numbers,
-                            a: int | float,
-                            b: int | float) -> list[Solution | None]:
-    solutions = []
+class NumbersPair(NamedTuple):
+    a: float | int
+    b: float | int
 
+
+def get_all_solutions_for_side(equation: Callable[[float | int, float | int], float | int],
+                               limitation: LimitationsFor2Numbers,
+                               a: int | float,
+                               b: int | float,
+                               side_a: bool = True) -> list[Solution | None]:
     if float(a).is_integer():
         a = int(a)
-
     if float(b).is_integer():
         b = int(b)
 
-    match b:
-        case int():
-            if float(a).is_integer():
-                if limitation.numbers_fits_limitation(a, b):
-                    solutions.append(Solution(
-                        a=a,
-                        b=b,
-                        result=equation(a, b)
-                    ))
-            else:
-                right_a_border = math.ceil(a)
-                right_b1, right_b2 = limitation.get_possible_b(right_a_border)
-                left_a_border = math.floor(a)
-                left_b1, left_b2 = limitation.get_possible_b(left_a_border)
-
-                if float(right_b1).is_integer():
-                    if limitation.numbers_fits_limitation(right_a_border, right_b1):
-                        solutions.append(Solution(
-                            a=right_a_border,
-                            b=right_b1,
-                            result=equation(right_a_border, right_b1)
-                        ))
-                    else:
-                        branch_solutions = get_all_solutions_for_b(equation, limitation, right_a_border, right_b1)
-                        for solution in branch_solutions:
-                            solutions.append(solution)
-                else:
-                    branch_solutions = [
-                        *get_all_solutions_for_b(equation, limitation, right_a_border, math.ceil(right_b1)),
-                        *get_all_solutions_for_b(equation, limitation, right_a_border, math.floor(right_b1)),
-                    ]
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-
-                if float(right_b2).is_integer():
-                    if limitation.numbers_fits_limitation(right_a_border, right_b2):
-                        solutions.append(Solution(
-                            a=right_a_border,
-                            b=right_b2,
-                            result=equation(right_a_border, right_b2)
-                        ))
-                    else:
-                        branch_solutions = get_all_solutions_for_b(equation, limitation, right_a_border, right_b2)
-                        for solution in branch_solutions:
-                            solutions.append(solution)
-                else:
-                    branch_solutions = [
-                        *get_all_solutions_for_b(equation, limitation, right_a_border, math.ceil(right_b2)),
-                        *get_all_solutions_for_b(equation, limitation, right_a_border, math.floor(right_b2)),
-                    ]
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-
-                if float(left_b1).is_integer():
-                    if limitation.numbers_fits_limitation(left_a_border, left_b1):
-                        solutions.append(Solution(
-                            a=left_a_border,
-                            b=left_b1,
-                            result=equation(left_a_border, left_b1)
-                        ))
-                    else:
-                        branch_solutions = get_all_solutions_for_b(equation, limitation, left_a_border, left_b1)
-                        for solution in branch_solutions:
-                            solutions.append(solution)
-                else:
-                    branch_solutions = [
-                        *get_all_solutions_for_b(equation, limitation, left_a_border, math.ceil(left_b1)),
-                        *get_all_solutions_for_b(equation, limitation, right_a_border, math.floor(left_b1)),
-                    ]
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-
-                if float(left_b2).is_integer():
-                    if limitation.numbers_fits_limitation(left_a_border, left_b2):
-                        solutions.append(Solution(
-                            a=left_a_border,
-                            b=left_b2,
-                            result=equation(left_a_border, left_b2)
-                        ))
-                    else:
-                        branch_solutions = get_all_solutions_for_b(equation, limitation, left_a_border, left_b2)
-                        for solution in branch_solutions:
-                            solutions.append(solution)
-                else:
-                    branch_solutions = [
-                        *get_all_solutions_for_b(equation, limitation, left_a_border, math.ceil(left_b2)),
-                        *get_all_solutions_for_b(equation, limitation, right_a_border, math.floor(left_b2)),
-                    ]
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-        case float():
-            right_b_border = math.ceil(b)
-            right_a1, right_a2 = limitation.get_possible_a(right_b_border)
-            left_b_border = math.floor(b)
-            left_a1, left_a2 = limitation.get_possible_a(left_b_border)
-
-            if float(right_a1).is_integer():
-                if limitation.numbers_fits_limitation(right_a1, right_b_border):
-                    solutions.append(Solution(
-                        a=right_a1,
-                        b=right_b_border,
-                        result=equation(right_a1, right_b_border)
-                    ))
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, right_b_border, math.ceil(right_a1)),
-                    *get_all_solutions_for_a(equation, limitation, right_b_border, math.floor(right_a1)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
-
-            if float(right_a2).is_integer():
-                if limitation.numbers_fits_limitation(right_a2, right_b_border):
-                    solutions.append(Solution(
-                        a=right_a2,
-                        b=right_b_border,
-                        result=equation(right_a2, right_b_border)
-                    ))
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, right_b_border, math.ceil(right_a1)),
-                    *get_all_solutions_for_a(equation, limitation, right_b_border, math.floor(right_a1)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
-
-            if float(left_a1).is_integer():
-                if limitation.numbers_fits_limitation(left_a1, left_b_border):
-                    solutions.append(Solution(
-                        a=left_a1,
-                        b=left_b_border,
-                        result=equation(right_a1, left_b_border)
-                    ))
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, left_b_border, math.ceil(left_a1)),
-                    *get_all_solutions_for_a(equation, limitation, left_b_border, math.floor(left_a1)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
-
-            if float(left_a2).is_integer():
-                if limitation.numbers_fits_limitation(left_a2, left_b_border):
-                    solutions.append(Solution(
-                        a=left_a2,
-                        b=left_b_border,
-                        result=equation(left_a2, left_b_border)
-                    ))
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, left_b_border, math.ceil(left_a2)),
-                    *get_all_solutions_for_a(equation, limitation, left_b_border, math.floor(left_a2)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
-
-    return solutions
-
-
-def get_all_solutions_for_a(equation: Callable[[float | int, float | int], float | int],
-                            limitation: LimitationsFor2Numbers,
-                            a: int | float,
-                            b: int | float) -> list[Solution | None]:
+    a_in_priority = None
     solutions = []
 
-    if float(a).is_integer():
-        a = int(a)
+    match a, b:
+        case int(), int() if limitation.numbers_fits_limitation(a, b):
+            solutions.append(Solution(
+                a=a,
+                b=b,
+                result=equation(a, b)
+            ))
+        case int(), float() if side_a:
+            a_in_priority = True
+        case float(), int() if not side_a:
+            a_in_priority = False
+        case float(), _ if side_a:
+            a_in_priority = False
+        case _, float() if not side_a:
+            a_in_priority = True
 
-    if float(b).is_integer():
-        b = int(b)
+    if a_in_priority is None:
+        return solutions
+    elif a_in_priority:
+        right_b_border = math.ceil(b)
+        right_a1, right_a2 = limitation.get_possible_a(right_b_border)
+        left_b_border = math.floor(b)
+        left_a1, left_a2 = limitation.get_possible_a(left_b_border)
 
-    match a:
-        case int():
-            if float(b).is_integer():
-                if limitation.numbers_fits_limitation(a, b):
-                    solutions.append(Solution(
-                        a=a,
-                        b=b,
-                        result=equation(a, b)
-                    ))
+        number_pairs = [
+            NumbersPair(a=right_a1, b=right_b_border),
+            NumbersPair(a=right_a2, b=right_b_border),
+            NumbersPair(a=left_a1, b=left_b_border),
+            NumbersPair(a=left_a2, b=left_b_border)
+        ]
+    else:
+        right_a_border = math.ceil(a)
+        right_b1, right_b2 = limitation.get_possible_b(right_a_border)
+        left_a_border = math.floor(a)
+        left_b1, left_b2 = limitation.get_possible_b(left_a_border)
+
+        number_pairs = [
+            NumbersPair(a=right_a_border, b=right_b1),
+            NumbersPair(a=right_a_border, b=right_b2),
+            NumbersPair(a=left_a_border, b=left_b1),
+            NumbersPair(a=left_a_border, b=left_b2)
+        ]
+
+    for number_a, number_b in number_pairs:
+        if float(number_a if a_in_priority else number_b).is_integer():
+            if limitation.numbers_fits_limitation(number_a, number_b):
+                solutions.append(Solution(
+                    a=number_a,
+                    b=number_b,
+                    result=equation(number_a, number_b)
+                ))
             else:
-                right_b_border = math.ceil(b)
-                right_a1, right_a2 = limitation.get_possible_a(right_b_border)
-                left_b_border = math.floor(b)
-                left_a1, left_a2 = limitation.get_possible_a(left_b_border)
-
-                if float(right_a1).is_integer():
-                    if limitation.numbers_fits_limitation(right_a1, right_b_border):
-                        solutions.append(Solution(
-                            a=right_a1,
-                            b=right_b_border,
-                            result=equation(right_a1, right_b_border)
-                        ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, right_a1, right_b_border)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-
-                if float(right_a2).is_integer():
-                    if limitation.numbers_fits_limitation(right_a2, right_b_border):
-                        solutions.append(Solution(
-                            a=right_a2,
-                            b=right_b_border,
-                            result=equation(right_a2, right_b_border)
-                        ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, right_a2, right_b_border)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-
-                if float(left_a1).is_integer():
-                    if limitation.numbers_fits_limitation(left_a1, left_b_border):
-                        solutions.append(Solution(
-                            a=left_a1,
-                            b=left_b_border,
-                            result=equation(right_a1, left_b_border)
-                        ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, left_a1, left_b_border)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-
-                if float(left_a2).is_integer():
-                    if limitation.numbers_fits_limitation(left_a2, left_b_border):
-                        solutions.append(Solution(
-                            a=left_a2,
-                            b=left_b_border,
-                            result=equation(left_a2, left_b_border)
-                        ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, left_a2, left_b_border)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-        case float():
-            right_a_border = math.ceil(a)
-            right_b1, right_b2 = limitation.get_possible_b(right_a_border)
-            left_a_border = math.floor(a)
-            left_b1, left_b2 = limitation.get_possible_b(left_a_border)
-
-            if float(right_b1).is_integer():
-                if limitation.numbers_fits_limitation(right_a_border, right_b1):
-                    solutions.append(Solution(
-                        a=right_a_border,
-                        b=right_b1,
-                        result=equation(right_a_border, right_b1)
-                    ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, right_a_border, right_b1)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, right_a_border, math.ceil(right_b1)),
-                    *get_all_solutions_for_a(equation, limitation, right_a_border, math.floor(right_b1)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
-
-            if float(right_b2).is_integer():
-                if limitation.numbers_fits_limitation(right_a_border, right_b2):
-                    solutions.append(Solution(
-                        a=right_a_border,
-                        b=right_b2,
-                        result=equation(right_a_border, right_b2)
-                    ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, right_a_border, right_b2)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, right_a_border, math.ceil(right_b2)),
-                    *get_all_solutions_for_a(equation, limitation, right_a_border, math.floor(right_b2)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
-
-            if float(left_b1).is_integer():
-                if limitation.numbers_fits_limitation(left_a_border, left_b1):
-                    solutions.append(Solution(
-                        a=left_a_border,
-                        b=left_b1,
-                        result=equation(left_a_border, left_b1)
-                    ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, left_a_border, left_b1)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, left_a_border, math.ceil(left_b1)),
-                    *get_all_solutions_for_a(equation, limitation, left_a_border, math.floor(left_b1)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
-
-            if float(left_b2).is_integer():
-                if limitation.numbers_fits_limitation(left_a_border, left_b2):
-                    solutions.append(Solution(
-                        a=left_a_border,
-                        b=left_b2,
-                        result=equation(left_a_border, left_b2)
-                    ))
-                else:
-                    branch_solutions = get_all_solutions_for_a(equation, limitation, left_a_border, left_b2)
-                    for solution in branch_solutions:
-                        solutions.append(solution)
-            else:
-                branch_solutions = [
-                    *get_all_solutions_for_a(equation, limitation, left_a_border, math.ceil(left_b2)),
-                    *get_all_solutions_for_a(equation, limitation, left_a_border, math.floor(left_b2)),
-                ]
-                for solution in branch_solutions:
-                    solutions.append(solution)
+                solutions += get_all_solutions(equation, limitation, number_a, number_b)
+        else:
+            solutions += get_all_solutions(equation, limitation, number_a,
+                                           math.ceil(number_a if a_in_priority else number_b))
+            solutions += get_all_solutions(equation, limitation, number_a,
+                                           math.floor(number_a if a_in_priority else number_b))
     return solutions
 
 
@@ -356,8 +106,8 @@ def get_all_solutions(equation: Callable[[float | int, float | int], float | int
                       a: int | float,
                       b: int | float) -> list[Solution | None]:
     return [
-        *get_all_solutions_for_a(equation, limitation, a, b),
-        *get_all_solutions_for_b(equation, limitation, a, b)
+        *get_all_solutions_for_side(equation, limitation, a, b, True),
+        *get_all_solutions_for_side(equation, limitation, a, b, False)
     ]
 
 
