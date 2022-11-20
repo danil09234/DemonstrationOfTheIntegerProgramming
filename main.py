dcf135093569587fb56628f3d3d541377f3ad7ed
danil09234 <dailzag05@gmail.com>
2022-11-20 01:17:02 +0100

    Documentation of a code

--- main.py
+++ main.py
@@ -5,20 +5,34 @@ from typing import Callable, NamedTuple
 
 @dataclass
 class LimitationsFor2Numbers:
+    """
+    Limitations for an equation with two arguments
+    """
+    # Optimal "a" argument, which fits limitations
     optimal_a: int | float
+    # Optimal "b" argument, which fits limitations
     optimal_b: int | float
+    # Function to get if an argument pairs fits limitations
     numbers_fits_limitation: Callable[[float | int, float | int], bool]
+    # Function to get a list of 2 possible a by putting b into an equation
     get_possible_a: Callable[[float | int], list[float | int, float | int]]
+    # Function to get a list of 2 possible b by putting a into an equation
     get_possible_b: Callable[[float | int], list[float | int, float | int]]
 
 
 class Solution(NamedTuple):
-    a: float | int
-    b: float | int
-    result: float | int
+    """
+    Solution of an equation
+    """
+    a: float | int  # Equation argument "a"
+    b: float | int  # Equation argument "b"
+    result: float | int  # Result of equation with arguments "a" and "b"
 
 
 class NumbersPair(NamedTuple):
+    """
+    Pairs of equation arguments
+    """
     a: float | int
     b: float | int
 
@@ -27,7 +41,18 @@ def get_all_solutions_for_side(equation: Callable[[float | int, float | int], fl
                                limitation: LimitationsFor2Numbers,
                                a: int | float,
                                b: int | float,
-                               side_a: bool = True) -> list[Solution | None]:
+                               side_a: bool = True) -> list[Solution]:
+    """
+    Function to get a list of all solutions of an equation by a certain side.
+    Each solution will be of an integer "a", integer "b" and integer "result".
+    Warning: integer means a number, not a type
+    :param equation: Equation to work with
+    :param limitation: Limitations for the equation arguments
+    :param a: Optimal a for the equation, which fits limitations
+    :param b: Optimal b for the equation, which fits limitations
+    :param side_a: If True, function will work with a side in a priority. Else b side in a priority
+    :return: List of Solutions
+    """
     if float(a).is_integer():
         a = int(a)
     if float(b).is_integer():
@@ -98,6 +123,11 @@ def get_all_solutions_for_side(equation: Callable[[float | int, float | int], fl
 
 
 def get_max_solution(solutions: list[Solution]):
+    """
+    Get max Solution object by result key
+    :param solutions: A list to find max
+    :return: Solution with a max result
+    """
     return max(solutions, key=lambda solution: solution.result)
 
 
@@ -105,6 +135,16 @@ def get_all_solutions(equation: Callable[[float | int, float | int], float | int
                       limitation: LimitationsFor2Numbers,
                       a: int | float,
                       b: int | float) -> list[Solution | None]:
+    """
+    Function to get a list of all solutions of an equation by each side.
+    Each solution will be of an integer "a", integer "b" and integer "result".
+    Warning: integer means a number, not a type
+    :param equation: Equation to work with
+    :param limitation: Limitations for the equation arguments
+    :param a: Optimal a for the equation, which fits limitations
+    :param b: Optimal b for the equation, which fits limitations
+    :return: A list of all equation solutions
+    """
     return [
         *get_all_solutions_for_side(equation, limitation, a, b, True),
         *get_all_solutions_for_side(equation, limitation, a, b, False)
@@ -114,6 +154,13 @@ def get_all_solutions(equation: Callable[[float | int, float | int], float | int
 def integer_division_for_an_equation_with_2_unknowns(
         equation: Callable[[float | int, float | int], float | int],
         limitations: LimitationsFor2Numbers) -> Solution:
+    """
+    Function to find a maximal integer solution with integer "a" and "b" arguments for a given equation
+    Arguments pair will fit limitations
+    :param equation: Equation to find an integer solution
+    :param limitations: Limitations for the equation arguments
+    :return: Maximal integer solution
+    """
     all_solutions = get_all_solutions(equation, limitations, limitations.optimal_a, limitations.optimal_b)
 
     return get_max_solution(all_solutions)
